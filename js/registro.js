0ffbef6dbc4361c23a6ec61acea65e9d9c988632
David Morales <dmoralesh1989@gmail.com>
2023-07-28 23:15:50 -0600

    first commit

---
+++ js/registro.js
@@ -0,0 +1,22 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) => {
+    e.preventDefault();
+  const respuesta = await fetch('https://sheet.best/api/sheets/93bce439-fb04-4ca3-80a4-9dd0021046f8', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Nombre": formulario.nombre.value,
+            "confirmacion": formulario.confirmacion.value,
+            "Telefono": formulario.telefono.value,
+        })
+    });
+// Leer Filas
+    registro.classList.remove('activo');
+    exito.classList.add('activo'); 
+});
